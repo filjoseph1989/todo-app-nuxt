853c84f0a876836b65e937c95c47398ccf355e7c
fil <filjoseph22@gmail.com>
2024-06-22 19:33:34 +0800

    code optimized

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -6,31 +6,28 @@ export default defineNuxtPlugin((nuxtApp) => {
     type SetUserId = (id: string) => void;
     type GetToken = () => string;
 
+    const cookieOptions = {
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: true,
+        maxAge: 3600,
+        httpOnly: true
+    };
+
     const setAuthToken: SetAuthToken = (token: string) => {
         if (getToken()) {
             nuxtApp.$graphql.default.setHeader('Authorization', `Bearer ${getToken()}`);
             return;
         }
-        const authToken = useCookie('authToken', {
-            path: '/',
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: true,
-            maxAge: 3600,
-            httpOnly: true
-        })
+        
+        const authToken = useCookie('authToken', cookieOptions)
         authToken.value = token;
 
         nuxtApp.$graphql.default.setHeader('Authorization', `Bearer ${authToken.value}`);
     };
 
     const setUserId: SetUserId = (id: string) => {
-        const userId = useCookie('userId', {
-            path: '/',
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: true,
-            maxAge: 3600,
-            httpOnly: true
-        })
+        const userId = useCookie('userId', cookieOptions)
         userId.value = id;
     };
 
