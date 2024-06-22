00556b4d4ca3009166d1c78ce2b76222c7d3df21
fil <filjoseph22@gmail.com>
2024-06-23 00:03:11 +0800

    move to the center, the form

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,31 @@
 <template>
-  <v-container>
-    <v-form @submit.prevent="login">
-      <v-text-field
-        v-model="email"
-        label="Email"
-        type="text"
-        name="email"
-        required
-        autocomplete="email"
-      ></v-text-field>
-      <v-text-field
-        v-model="password"
-        label="Password"
-        type="password"
-        name="password"
-        required
-        autocomplete="current-password"
-      ></v-text-field>
-      <v-btn type="submit" color="primary">Login</v-btn>
-    </v-form>
+  <v-container
+    class="d-flex align-center justify-center"
+    style="height: 100vh;">
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-form @submit.prevent="login" method="post">
+          <v-text-field
+            v-model="email"
+            label="Email"
+            type="text"
+            name="email"
+            required
+            autocomplete="email"
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Password"
+            type="password"
+            name="password"
+            required
+            autocomplete="current-password"
+          ></v-text-field>
+          <v-btn type="submit" color="primary">Login</v-btn>
+          <v-btn type="button" color="primary" class="ml-1">Register</v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
